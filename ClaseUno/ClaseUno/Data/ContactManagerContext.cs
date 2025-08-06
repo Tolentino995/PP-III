03cc80cd24fe8d0b76360fd3952303fe53442fbd
Anthony Tolentino <tolentino.git@gmail.com>
2025-08-06 19:36:50 -0300

    Primera calse seguimos el pdf que nos compartio, se continuo la otra clase

---
+++ ClaseUno/ClaseUno/Data/ContactManagerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ClaseUno.Model;
+
+namespace ClaseUno.Data
+
+{
+    public class ContactManagerContext : DbContext
+    {
+        public ContactManagerContext(DbContextOptions<ContactManagerContext>
+        options)
+        : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ ClaseUno/ClaseUno/Model/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ClaseUno.Model
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+    }
+}
